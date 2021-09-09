180c0515852f6a96aa0fb0239e99d1da06a391e1
Francisco Garcia <francisco.garcia@globant.com>
2021-09-08 21:44:57 -0500

    handlebars example

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const port = 8080;
 
 //servir contenido estatico
 
+app.set('view engine', 'hbs')
+
 app.use( express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    nombre: 'Francisco Garcia',
+    titulo: 'Curso node'
+  })
+})
+
 
 app.get('/generic', (req, res) => {
   res.sendFile(__dirname + '/public/generic.html')
